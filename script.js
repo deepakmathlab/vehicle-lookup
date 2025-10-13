ff08707b9969fbd5223831cc01dce5ef9afeb317
deepakmathlab <deepakmathlab@gmail.com>
2025-10-13 11:34:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const vehicles = [
-  {plate:"KA01AB1234",owner:"Deepak S",rc:"RC-65783",insurance:"INS-98342",puc:"PUC-78234",expiry_rc:"2026-05-10",expiry_insurance:"2025-12-30",expiry_puc:"2024-11-01",notes:"Valid"},
-  {plate:"KA02BC5678",owner:"Anjali Rao",rc:"RC-90873",insurance:"INS-56421",puc:"PUC-99913",expiry_rc:"2027-02-18",expiry_insurance:"2025-08-14",expiry_puc:"2024-10-12",notes:"Valid"},
-  {plate:"KA03CD9876",owner:"Rahul Mehta",rc:"RC-23489",insurance:"INS-99822",puc:"PUC-22011",expiry_rc:"2025-09-01",expiry_insurance:"2025-04-20",expiry_puc:"2024-12-05",notes:"Insurance Expiring Soon"},
-  {plate:"KA04EF3456",owner:"Sneha Kumar",rc:"RC-55567",insurance:"INS-78312",puc:"PUC-67342",expiry_rc:"2026-07-22",expiry_insurance:"2025-09-09",expiry_puc:"2025-03-15",notes:"PUC Expiring Soon"},
-  {plate:"KA05GH7654",owner:"Arun Nair",rc:"RC-88888",insurance:"INS-99001",puc:"PUC-33442",expiry_rc:"2025-06-12",expiry_insurance:"2025-10-11",expiry_puc:"2024-10-15",notes:"Valid"}
+  {plate:"KA17HN6465",owner:"Deepak S",rc:"RC-65783",insurance:"INS-98342",expiry_rc:"2026-05-10",expiry_insurance:"2025-12-30",notes:"Valid"},
+  {plate:"KA17HS5539",owner:"Bharath B S",rc:"RC-90873",insurance:"INS-56421",expiry_rc:"2027-02-18",expiry_insurance:"2025-08-14",notes:"Valid"},
+  {plate:"KA17HB6062",owner:"Nandan G S",rc:"RC-23489",insurance:"INS-99822",expiry_rc:"2025-09-01",expiry_insurance:"2025-04-20",notes:"Insurance Expiring Soon"},
+  {plate:"KA04EF3456",owner:"Sneha Kumar",rc:"RC-55567",insurance:"INS-78312",expiry_rc:"2026-07-22",expiry_insurance:"2025-09-09",notes:"PUC Expiring Soon"},
+  {plate:"KA05GH7654",owner:"Arun Nair",rc:"RC-88888",insurance:"INS-99001",expiry_rc:"2025-06-12",expiry_insurance:"2025-10-11",notes:"Valid"}
 ];
 
 function normalize(s){return s.replace(/\s+/g,"").toUpperCase();}
@@ -31,3 +31,4 @@ function displayTable(arr){
   html+="</table>";
   document.getElementById("output").innerHTML = html;
 }
+
